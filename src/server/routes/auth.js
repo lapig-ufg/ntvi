f6f5f5247e8f8fe289bc13a7c980f3f143dd086a
Tharles de Sousa Andrade <irtharles@gmail.com>
2020-12-09 09:05:58 -0300

    config oatuh

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     var control = app.controllers.auth;
 
     app.post('/api/auth/login', control.login);
-    app.post('/api/auth/oauth', control.oauth);
     app.post('/api/auth/register', control.register);
+    app.get('/api/auth/oauth', control.oauth);
     app.get('/api/auth/logout', control.logout);
 }
