b8a8e8d36c92f63a0d6866b969af84ccd4a8bdc4
Tharles de Sousa Andrade <irtharles@gmail.com>
2020-12-11 17:47:48 -0300

    fix unused modules

--- src/client/src/app/pages/use-class/use-class-routing.module.ts
+++ src/client/src/app/pages/use-class/use-class-routing.module.ts
@@ -5,13 +5,24 @@ import { ViewComponent } from './view/view.component';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [
-  { path: 'class', redirectTo: 'class/index', pathMatch: 'full'},
-  { path: 'class/index', component: IndexComponent },
-  { path: 'class/:classId/view', component: ViewComponent },
-  { path: 'class/create', component: CreateComponent },
-  { path: 'class/:classId/edit', component: EditComponent },
-];
+const routes: Routes = [{
+  path: '',
+  component: IndexComponent,
+  children: [
+    {
+      path: '/:classId/view',
+      component: ViewComponent,
+    },
+    {
+      path: '/create',
+      component: CreateComponent,
+    },
+    {
+      path: '/:classId/edit',
+      component: EditComponent,
+    },
+  ],
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
