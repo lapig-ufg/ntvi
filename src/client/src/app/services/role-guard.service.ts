b84e2fc1bcab2f6c3d7fd29af62e304bd8bb6ea3
FernandaStefani <fer.stefani.souza@gmail.com>
2020-12-14 15:59:08 -0300

    role guard

--- src/client/src/app/services/role-guard.service.ts
+++ src/client/src/app/services/role-guard.service.ts
@@ -15,10 +15,10 @@ export class RoleGuardService implements  CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
     this.allowedRoles = route.data['roles'];
-    let currentUser = JSON.parse(localStorage.getItem('user'));
-    const roleDefault = 'DEFAULT'
-    
-    const allowed = this.allowedRoles.includes((currentUser === undefined || currentUser === null) ? roleDefault : currentUser.role);
+    const currentUser = JSON.parse(localStorage.getItem('user'));
+    const roleDefault = 'DEFAULT';
+    const allowed = this.allowedRoles.includes(
+      (currentUser === undefined || currentUser === null) ? roleDefault : currentUser.role);
     if (!allowed) {
       this.showToast('warning', 'top-right');
     }
