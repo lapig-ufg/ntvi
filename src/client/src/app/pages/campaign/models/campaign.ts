e7da6f484416f44eb7080c711bb9d9128420fefb
Tharles de Sousa Andrade <irtharles@gmail.com>
2020-12-17 11:50:11 -0300

    update campaign service

--- src/client/src/app/pages/campaign/models/campaign.ts
+++ src/client/src/app/pages/campaign/models/campaign.ts
@@ -5,17 +5,18 @@ import { Image } from './Image';
 import { Point } from './point';
 
 export interface Campaign {
-  id: number;
+  id?: number;
   name: string;
   description: string;
   numInspectors: number;
-  typePeriod: string;
-  initialDate: Date;
-  finalDate: Date;
-  publish: boolean;
-  organization: Organization;
-  classes: UseClass[];
-  compositions: Composition[];
-  points: Point[];
-  images: Image[];
+  typePeriod?: string;
+  status?: string;
+  initialDate?: Date;
+  finalDate?: Date;
+  publish?: boolean;
+  organization?: Organization;
+  classes?: UseClass[];
+  compositions?: Composition[];
+  points?: Point[];
+  images?: Image[];
 }
