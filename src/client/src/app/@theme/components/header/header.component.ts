a618084e9810c37bed18907cf4397eeb55f672a1
FernandaStefani <fer.stefani.souza@gmail.com>
2020-12-17 10:00:16 -0300

    tela profile em construção

--- src/client/src/app/@theme/components/header/header.component.ts
+++ src/client/src/app/@theme/components/header/header.component.ts
@@ -120,12 +120,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.router.navigate(['auth/login']);
   }
 
+  profile () {
+    this.router.navigate(['pages/users/profile']);
+  }
+
   handleMenu(evt) {
     const self = this;
     this.menuService.onItemClick().subscribe(result => {
       const { item } = result;
       if (item.title === 'Log out') {
         self.logout();
+      } else {
+        self.profile();
       }
     });
   }
