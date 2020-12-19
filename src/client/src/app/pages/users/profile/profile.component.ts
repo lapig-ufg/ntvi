137543fd389e96ce86041caff4d7cdc777dc0df4
FernandaStefani <fer.stefani.souza@gmail.com>
2020-12-19 18:19:35 -0300

    Lint

--- src/client/src/app/pages/users/profile/profile.component.ts
+++ src/client/src/app/pages/users/profile/profile.component.ts
@@ -33,7 +33,7 @@ export class ProfileComponent implements OnInit {
       this.selectedItem = data.organization.id.toString();
       this.user = data;
     });
-    
+
     this.organizationService.getAll().subscribe((data: Organization[]) => {
       this.organizations = data;
     });
