08f30092693f2b117ac35e11d30e67daece98ec9
FernandaStefani <fer.stefani.souza@gmail.com>
2020-12-19 18:14:03 -0300

    Edição Profile

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,23 +1,31 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
+const CryptoJS = require("crypto-js");
 
 module.exports = function (app) {
     var Controller = {}
-
+    let language = app.util.language;
     const prisma = new PrismaClient()
 
     Controller.getUserByID = async function (request, response) {
 
-        const {user} = request;
+        const {id} = request.params;
         // const { id } = request.params
         try {
             const us = await prisma.user.findUnique({
                 select: {
                     id: true,
                     name: true,
-                    organization:true
+                    email: true,
+                    city: true,
+                    state: true,
+                    country: true,
+                    geeKey: true,
+                    typeUser: true,
+                    picture: true,
+                    organization:true,
                 },
                 where: {
-                    id: parseInt(user.id),
+                    id: parseInt(id),
                 },
             });
             response.json(us)
@@ -41,6 +49,55 @@ module.exports = function (app) {
             console.error(e)
         }
 
+    }
+
+    Controller.updateUser = async function (request, response) {
+        const { id } = request.params
+        const { name, email, password, city, state, country, geeKey, organization } = request.body
+        let { lang } = request.headers;
+        const texts = language.getLang(lang);
+
+        try {
+            let arrayQueries = [];
+
+            if(password){
+                const hash = CryptoJS.MD5(password).toString();
+
+                arrayQueries.push(prisma.user.update({
+                    where: { id: parseInt(id) },
+                    data: {
+                        name: name,
+                        email: email != null ? email : undefined,
+                        password: hash,
+                        city: city != null ? email : undefined,
+                        state: state != null ? state : undefined,
+                        country: country != null ? country : undefined,
+                        geeKey: geeKey != null ? geeKey : undefined,
+                        organization: { connect: {id: parseInt(organization)} },
+                    }
+                }))
+            }else{
+                arrayQueries.push(prisma.user.update({
+                    where: { id: parseInt(id) },
+                    data: {
+                        name: name,
+                        email: email != null ? email : undefined,
+                        city: city != null ? email : undefined,
+                        state: state != null ? state : undefined,
+                        country: country != null ? country : undefined,
+                        geeKey: geeKey != null ? geeKey : undefined,
+                        organization: { connect: {id: parseInt(organization)} },
+                    }
+                }))
+            }
+
+            const resultQueries = await prisma.$transaction(arrayQueries)
+
+            response.status(200).json(resultQueries);
+        } catch (e) {
+            console.error(e)
+            response.status(500).json({ message: texts.login_msg_erro + e + '.' });
+        }
     }
 
     return Controller;
