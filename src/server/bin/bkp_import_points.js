8d8e2de6ff00499b7d07a55931e11a6d12304af1
luizmlpascoal <luizmlpascoal@gmail.com>
2020-12-19 21:41:33 -0300

    import points

--- src/server/bin/bkp_import_points.js
+++ src/server/bin/bkp_import_points.js
@@ -2,28 +2,17 @@ var fs = require("fs");
 var async = require("async");
 var mongodb = require('mongodb');
 var exec = require('child_process').exec;
-const { PrismaClient } = require('@prisma/client');
-const { runInContext } = require("vm");
 
-//var geojsonFile = process.argv[2];
-var campaign = process.argv[2];
-// var numInspec = process.argv[4];
-// var password = (process.argv[5] == null) ? null : process.argv[5];
-// var initialYear = (process.argv[6] == null) ? 1985 : process.argv[6];
-// var finalYear = (process.argv[7] == null) ? 2018 : process.argv[7];
-
-var rootFolder = ""
-var config = require('../config.js')()
+var geojsonFile = process.argv[2];
+var campaign = process.argv[3];
+var numInspec = process.argv[4];
+var password = (process.argv[5] == null) ? null : process.argv[5];
+var initialYear = (process.argv[6] == null) ? 1985 : process.argv[6];
+var finalYear = (process.argv[7] == null) ? 2018 : process.argv[7];
 
 var collectionPointsName = "points";
 var collectionCampaignName = "campaign";
-var dbUrl = 'mongodb://' + config.mongo.host + ':' + config.mongo.port + '/' + config.mongo.dbname;
-
-const prisma = new PrismaClient({
-	errorFormat: 'pretty',
-	log: ['query'],
-})
-
+var dbUrl = 'mongodb://localhost:27017/tvi';
 
 var checkError = function (error) {
 	if (error) {
@@ -33,25 +22,20 @@ var checkError = function (error) {
 }
 
 var getCoordinates = function (geojsonDataStr) {
-	// geojsonData = JSON.parse(geojsonDataStr)
-
-	geojsonData = geojsonDataStr
+	geojsonData = JSON.parse(geojsonDataStr)
 
 	var coordinates = []
-	for (var i = 0; i < geojsonData.points.length; i++) {
-
-		var dado = JSON.parse(geojsonData.points[i])
-
+	for (var i = 0; i < geojsonData.features.length; i++) {
 		coordinates.push(
 			{
-				// "id": geojsonData.features[i].properties['id'],
-				"X": dado["coordinates"][0],
-				"Y": dado["coordinates"][1],
-				// "uf": geojsonData.features[i].properties['uf'],
-				// "county": geojsonData.features[i].properties['county'],
-				// "biome": geojsonData.features[i].properties['biome'],
-				// "countyCode": geojsonData.features[i].properties['countyCode'],
-				// "value": geojsonData.features[i].properties.value
+				"id": geojsonData.features[i].properties['id'],
+				"X": geojsonData.features[i].geometry.coordinates[0],
+				"Y": geojsonData.features[i].geometry.coordinates[1],
+				"uf": geojsonData.features[i].properties['uf'],
+				"county": geojsonData.features[i].properties['county'],
+				"biome": geojsonData.features[i].properties['biome'],
+				"countyCode": geojsonData.features[i].properties['countyCode'],
+				"value": geojsonData.features[i].properties.value
 			}
 		);
 	}
@@ -60,9 +44,8 @@ var getCoordinates = function (geojsonDataStr) {
 }
 
 var getInfoByRegionCmd = function (coordinate) {
-	regions = rootFolder + "SHP/regions.shp";
+	regions = "SHP/regions.shp";
 	sql = "select COD_MUNICI,BIOMA,UF,MUNICIPIO from regions where ST_INTERSECTS(Geometry,GeomFromText('POINT(" + coordinate.X + " " + coordinate.Y + ")',4326))"
-
 	return 'ogrinfo -q -geom=no -dialect sqlite -sql "' + sql + '" ' + regions;
 }
 
@@ -105,7 +88,7 @@ var getInfoByRegion = function (coordinate, callback) {
 }
 
 var getInfoByTileCmd = function (coordinate) {
-	tiles = rootFolder + "SHP/tiles.shp";
+	tiles = "SHP/tiles.shp";
 	sql = "select path,row from tiles where ST_INTERSECTS(Geometry,GeomFromText('POINT(" + coordinate.X + " " + coordinate.Y + ")',4326))"
 	return 'ogrinfo -q -geom=no -dialect sqlite -sql "' + sql + '" ' + tiles;
 }
@@ -140,7 +123,6 @@ var getInfoByTile = function (coordinate, callback) {
 
 var getDB = function (dbUrl, callback) {
 	var MongoClient = mongodb.MongoClient;
-
 	MongoClient.connect(dbUrl, function (err, db) {
 		if (err)
 			return console.dir(err);
@@ -150,117 +132,36 @@ var getDB = function (dbUrl, callback) {
 
 var insertCampaing = function (db) {
 	db.collection(collectionCampaignName, function (err, collectionCampaign) {
-
-
-		console.log(info)
 		var createCamp = {
 			"_id": campaign,
-			"initialYear": parseInt(info.initialyear),
-			"finalYear": parseInt(info.finalyear),
-			"password": "teste123",
-			"landUse": info.useclasses,
-			"numInspec": parseInt(info.numinspetores),
-			"username": info.username
+			"initialYear": parseInt(initialYear),
+			"finalYear": parseInt(finalYear),
+			"password": password,
+			"landUse": [
+				"Pastagem Natural",
+				"Vegetação nativa",
+				"Pastagem Cultivada",
+				"Não observado",
+				"Agricultura Anual",
+				"Em regeneração",
+				"Agricultura Perene",
+				"Mosaico de ocupação",
+				"Água",
+				"Solo Exposto",
+				"Cana-de-açucar",
+				"Desmatamento",
+				"Área urbana",
+				"Silvicultura"
+			],
+			"numInspec": parseInt(numInspec)
 		}
-
-		console.log(createCamp)
-
-
-		// [
-		// 	"Pastagem Natural",
-		// 	"Vegetação nativa",
-		// 	"Pastagem Cultivada",
-		// 	"Não observado",
-		// 	"Agricultura Anual",
-		// 	"Em regeneração",
-		// 	"Agricultura Perene",
-		// 	"Mosaico de ocupação",
-		// 	"Água",
-		// 	"Solo Exposto",
-		// 	"Cana-de-açucar",
-		// 	"Desmatamento",
-		// 	"Área urbana",
-		// 	"Silvicultura"
-		// ]
 
 		collectionCampaign.insertOne(createCamp);
 	})
 
 }
 
-var getCampaignInfo = async function () {
-	let result = {};
-
-	await prisma.$connect()
-	try {
-
-		var q = await prisma.$queryRaw(
-			'SELECT c.name, c.id, c."numInspectors" as numInspetores, EXTRACT(YEAR FROM "initialDate") as initialYear, EXTRACT(YEAR FROM "finalDate") as finalYear, ST_ASGEOJSON(ST_SetSRID(ST_MAKEPOINT( cast(p.longitude as float), cast(p.latitude as float)), 4674)) as geojson FROM public."Point" p inner join "Campaign" c on c.id = p."campaignId" where p."campaignId" = $1', parseInt(campaign)) // PostgreSQL variables, represented by $1 and $2
-
-		var q2 = await prisma.$queryRaw(
-			'select u."B" , use.name, c.id from "Campaign" c INNER JOIN "_CampaignToUseClass" u ON u."A" = c.id INNER JOIN "UseClass" use on u."B" = use.id where c.id = $1', parseInt(campaign))
-
-		var q3 = await prisma.$queryRaw(
-			'select  use.id as iduser, use.name, use."typeUser" as typeuser, c.id, u."typeUserInCampaign" as typeuserincampaign from "Campaign" c INNER JOIN "UsersOnCampaigns" u ON u."campaignId" = c.id INNER JOIN "User" use on u."userId" = use.id where c.id = $1 and u."typeUserInCampaign" <> $2', parseInt(campaign), 'ADMIN')
-
-
-		let vet = []
-		q.forEach(x => {
-			if (vet.length == 0) {
-				result = omit(x, ['geojson'])
-			}
-			vet.push(x.geojson)
-		})
-
-		result.points = vet;
-
-		vet = []
-		q2.forEach(x => {
-			vet.push(x.name)
-		})
-
-		result.useclasses = vet;
-
-		vet = []
-		q3.forEach(x => {
-			vet.push({
-				iduser: parseInt(x.iduser),
-				name: x.name,
-				typeuserincampaign: x.typeuserincampaign,
-				typeuser: x.typeuser
-			})
-		})
-
-		result.username = vet;
-
-
-	}
-	catch (e) {
-		throw e
-	}
-	finally {
-		await prisma.$disconnect()
-	}
-
-	// 'SELECT ST_ASGEOJSON(ST_MAKEPOINT( cast(longitude as float), cast(latitude as float))) as geojson FROM public."Point" where "campaignId" = $1', campaign
-
-	return result
-}
-
-function omit(obj, props) {
-	props = props instanceof Array ? props : [props]
-	return eval(`(({${props.join(',')}, ...o}) => o)(obj)`)
-}
-
-var info = {};
-async function rodar() {
-	info = await getCampaignInfo();
-}
-
-rodar().then(function (error, geojsonDataStr) {
-
-	geojsonDataStr = info;
-
+fs.readFile(geojsonFile, 'utf-8', function (error, geojsonDataStr) {
 	checkError(error);
 
 	getDB(dbUrl, function (db) {
@@ -307,11 +208,4 @@ rodar().then(function (error, geojsonDataStr) {
 			async.eachSeries(getCoordinates(geojsonDataStr), eachFn, onComplete);
 		});
 	});
-
-
-})
-	.catch(e => {
-		throw e
-	})
-
-
+});
