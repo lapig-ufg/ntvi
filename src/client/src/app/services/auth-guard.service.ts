069564713aaaebed53b79649ae6fda083bfd3078
Tharles de Sousa Andrade <irtharles@gmail.com>
2021-06-06 23:05:29 -0300

    eslint fixed 1

--- src/client/src/app/services/auth-guard.service.ts
+++ src/client/src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, Router,} from '@angular/router';
+import {CanActivate, Router} from '@angular/router';
 import { NbAuthService } from '@nebular/auth';
 import { tap } from 'rxjs/operators';
 import jwtDecode, { JwtPayload } from 'jwt-decode';
@@ -36,11 +36,11 @@ export class AuthGuard implements CanActivate {
               token = localStorage.getItem('token');
             } else {
               const temp = await self.authService.getToken().toPromise();
-              token = temp.getValue()
+              token = temp.getValue();
             }
             const payload = jwtDecode<JwtPayload>(token);
             const isExpired = Date.now() >= payload.exp * 1000;
-            if(isExpired){
+            if (isExpired) {
               await self.router.navigate(['auth/login']);
             }
           } else {
